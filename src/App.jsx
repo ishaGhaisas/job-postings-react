89db636ec755f2836e878f23a8d62948164e0839
Isha Ghaisas <ighaisas@iu.edu>
2025-05-23 11:09:11 -0400

    home page done

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,17 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
       <Navbar />
-
-      {/* <!-- Hero --> */}
       <Hero/>
-
-      {/* <!-- Developers and Employers --> */}
       <HomeCards />
-      {/* <!-- Browse Jobs --> */}
       <JobListings />
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-        >
-      </section>
+      <ViewAllJobs />
     </>
   )
 }
