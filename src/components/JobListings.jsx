59d0ba8b1902428e1258d01d05c110560cc2bfcb
Isha Ghaisas <ighaisas@iu.edu>
2025-06-02 10:00:51 -0400

    single job page added

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import JobListing from "./JobListing"
+import Spinner from "./Spinner";
 
 const JobListings = ({ isHome = false }) => {
     // const jobListings = isHome ? jobs.slice(0, 3) : jobs;
@@ -8,8 +9,9 @@ const JobListings = ({ isHome = false }) => {
 
     useEffect(() => {
         const fetchJobs = async () => {
+            const apiUrl = isHome ? 'http://localhost:8000/jobs?_limit=3' : 'http://localhost:8000/jobs'
             try {
-                const res = await fetch('http://localhost:8000/jobs');
+                const res = await fetch(apiUrl);
                 const data = await res.json();
                 setJobs(data);
             } catch (error) {
@@ -28,13 +30,14 @@ const JobListings = ({ isHome = false }) => {
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                     Browse Jobs
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {loading ? (<h2>Loading</h2>) : (<>
+
+                {loading ? (<Spinner loading={loading} />) :
+                    (<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                         {jobs.map((job) => {
                             return <JobListing key={job.id} job={job} />;
                         })}
-                    </>)}
-                </div>
+                    </div>)}
+
             </div>
         </section>
     )
