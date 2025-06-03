29494f02e2ae0cb10cfc56f5f410e06e4dfa49c2
Isha Ghaisas <ighaisas@iu.edu>
2025-06-03 10:43:00 -0400

    form to add jobs

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, {jobLoader} from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/add-job' element={<AddJobPage />} />
       <Route path='job/:id' element={<JobPage />} loader = {jobLoader}/>
       <Route path='*' element={<NotFoundPage />} />
     </Route>
